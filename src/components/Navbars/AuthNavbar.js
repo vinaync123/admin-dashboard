466ab53cd9758d735dd20e8ac1111a7805a04cd6
vinaychaudhari <vinay.chaudhari@incetius.com>
2021-06-12 09:24:38 +0530

    initial commit

---
+++ src/components/Navbars/AuthNavbar.js
@@ -0,0 +1,16 @@
+/*eslint-disable*/
+import React from "react";
+import { Link } from "react-router-dom";
+
+// components
+
+import PagesDropdown from "components/Dropdowns/PagesDropdown.js";
+
+export default function Navbar(props) {
+  const [navbarOpen, setNavbarOpen] = React.useState(false);
+  return (
+    <>
+  
+    </>
+  );
+}
